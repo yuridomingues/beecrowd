075e9d943508224a9f6e6e0513a90b4ed74c7b00
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-07-04 12:23:29 -0300

    initial

---
+++ diferenca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int A = 5;
+    int B = 6;
+    int C = 7; 
+    int D = 8;
+    
+    int DIFERENCA = (A * B - C * D);
+    printf("%d\n", DIFERENCA);
+    
+    return 0;
+}
