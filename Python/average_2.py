01d09b602df1490aef5ec3914c971b7eb1c73425
hachi <yuridomingues.contato@gmail.com>
2024-11-26 14:10:53 -0300

    feat: adiciona exercicio 1006

---
+++ Python/average_2.py
@@ -0,0 +1,19 @@
+"""
+This module calculates the average of three numbers.
+"""
+
+def calculate_average() -> None:
+    """
+    Calculate the average of three numbers and print the result.
+    """
+    a: float = float(input())
+    b: float = float(input())
+    c: float = float(input())
+
+    averaged_number: float = (a + b + c) / 2 + 3 + 5
+
+    formatted_average = f"{averaged_number:.1f}"
+    print("MEDIA =", formatted_average)
+
+
+calculate_average()
